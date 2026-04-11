4963c73200c1d2e05fe19883442767c7b216d5b6
talor1321 <106119007+talor1321@users.noreply.github.com>
2026-04-11 21:12:14 +0300

    updated

--- c_code.c
+++ c_code.c
@@ -76,7 +76,58 @@ uint8_t step_lfsr_high_hw(uint8_t* msg, uint8_t len)
     
     return 0x00;
 }
+//TODELETE
 
+static uint32_t lfsr_state_low_hw_m = 0; // המצב הממוסך
+static uint32_t lfsr_mask_low_hw = 0;    // המסכה
+
+uint8_t set_seed_masked_lfsr_low_hw(uint8_t* msg, uint8_t len)
+{
+    // אנחנו מצפים כעת ל-8 בתים: 4 הראשונים למצב הממוסך, 4 האחרונים למסכה
+    if(len >= 8) {
+        lfsr_state_low_hw_m = ((uint32_t)msg[3] << 24) | 
+                              ((uint32_t)msg[2] << 16) | 
+                              ((uint32_t)msg[1] << 8)  | 
+                              ((uint32_t)msg[0]);
+
+        lfsr_mask_low_hw = ((uint32_t)msg[7] << 24) | 
+                           ((uint32_t)msg[6] << 16) | 
+                           ((uint32_t)msg[5] << 8)  | 
+                           ((uint32_t)msg[4]);
+    }
+    
+    // ACK
+    uint8_t res = 0xCC; 
+    simpleserial_put('r', 1, &res); 
+    
+    return 0x00;
+}
+
+uint8_t step_masked_lfsr_low_hw(uint8_t* msg, uint8_t len)
+{
+    // חילוץ מספר הצעדים מההודעה
+    uint8_t num_steps = (len > 0) ? msg[0] : 1;
+
+    trigger_high(); 
+    
+    for(uint8_t i = 0; i < num_steps; i++) {
+        // שליפת ה-LSB משני החלקים
+        uint32_t lsb_m = lfsr_state_low_hw_m & 1;
+        uint32_t lsb_mask = lfsr_mask_low_hw & 1;
+
+        // קידום המצב הממוסך והמסכה במקביל
+        lfsr_state_low_hw_m = (lfsr_state_low_hw_m >> 1) ^ (0xE0000200 * lsb_m);
+        lfsr_mask_low_hw = (lfsr_mask_low_hw >> 1) ^ (0xE0000200 * lsb_mask);
+    }
+    
+    trigger_low(); 
+
+    // הסרת המיסוך לצורך החזרת התוצאה הנכונה (כדי לוודא שהפלט תקין מול הסקריפט)
+    uint8_t res = (uint8_t)((lfsr_state_low_hw_m ^ lfsr_mask_low_hw) & 0xFF);
+    simpleserial_put('r', 1, &res); 
+    
+    return 0x00;
+}
 int main(void)
 {
     platform_init();
@@ -88,6 +139,10 @@ int main(void)
     simpleserial_addcmd('b', 4, set_seed_lfsr_high_hw);
     simpleserial_addcmd('c', 1, step_lfsr_low_hw);
     simpleserial_addcmd('d', 1, step_lfsr_high_hw);
+
+    //TODELETE
+    simpleserial_addcmd('e', 8, set_seed_masked_lfsr_low_hw);
+    simpleserial_addcmd('f', 1, step_masked_lfsr_low_hw);
     
     while(1)
         simpleserial_get();
